e890563e1efc23d14a17a7745288318ef29a6522
Trend20 <enockomondi305@gmail.com>
2023-10-13 15:17:04 +0300

    adding book services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { BookModule } from './book/book.module';
@@ -8,7 +6,7 @@ import { OrderModule } from './order/order.module';
 
 @Module({
   imports: [UserModule, AuthModule, BookModule, OrderModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from 'src/types/book.model';
 
 @Injectable()
-export class BookService {}
+export class BookService {
+  books: Book[] = [];
+
+  // get all books
+  getAllBooks() {
+    return this.books;
+  }
+
+  // get book by id
+  getBookById(id: string): Book {
+    return this.books.find((book) => book.id === id);
+  }
+
+  // create book
+  // createBook(book: Book): Book {
+
+  // }
+
+  // update book
+
+  // delete book
+}
